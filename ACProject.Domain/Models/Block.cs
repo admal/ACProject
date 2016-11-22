48c4d3bef6b7bd0a24618fdbe27abaa06c90c70c
jedrekf <jedrek@fijalkowscy.com>
2016-11-22 01:05:16 +0100

    Block on initiation will remove unnecessary rows and columns and resize it's grid accordingly

--- ACProject.Domain/Models/Block.cs
+++ ACProject.Domain/Models/Block.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace ACProject.Domain.Models
     public class Block : IBlock
     {
         private static readonly Random rnd = new Random();
+        private int[,] _grid;
 
         public Block(int width, int height)
         {
@@ -23,10 +25,74 @@ namespace ACProject.Domain.Models
         {
             Color = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
             this.Grid = grid;
+            removeUselessRowsAndCols();
+        }
+        private void removeUselessRowsAndCols()
+        {
+            int countY = 0;
+            int[] necessaryRows = new int[_grid.GetLength(1)]; // necessary grid rows
+            int[] necessaryCols = new int[_grid.GetLength(0)]; // necessary grid cols
+            for (int i = 0; i < necessaryCols.Length; i++) necessaryCols[i] = 0;
+            for (int i = 0; i < necessaryRows.Length; i++) necessaryRows[i] = 0;
+
+            for (int i = 0; i < Grid.GetLength(0); i++) // po szerokosci
+            {
+                for (int j = 0; j < Grid.GetLength(1); j++) // po wysokosci
+                {
+                    if (this.Grid[i, j] == 1)
+                    {
+                        necessaryCols[i] = 1;
+                        necessaryRows[j] = 1;
+                    }
+                }
+            }
+
+            int countCols = 0;
+            int countRows = 0;
+            for (int i = 0; i < necessaryCols.Length; i++) // count sizes of a new grid
+            {
+                if(necessaryCols[i] == 1)
+                    countCols++;
+            }
+            for (int i = 0; i < necessaryRows.Length; i++)
+            {
+                if (necessaryRows[i] == 1)
+                    countRows++;
+            }
+            int[,] newGrid = new int[countCols, countRows];
+
+            int x = 0, y = 0;
+            for (int i = 0; i < necessaryRows.Length; i++)
+            {
+                if (necessaryRows[i] == 1)
+                {
+                    x = 0;
+                    for (int j = 0; j < necessaryCols.Length; j++)
+                    {
+                        if (necessaryCols[j] == 1 )
+                        {
+                            newGrid[x, y] = _grid[j, i];
+                            x++;
+                        }
+                    }
+                    y++;
+                }
+            }
+            _grid = newGrid;
         }
 
         public int Count { get; set; }
-        public int[,] Grid { get; set; }
+        public int[,] Grid {
+            get
+            {
+                return _grid;
+            }
+            set
+            {
+                _grid = value;
+                removeUselessRowsAndCols();
+            } 
+        }
         public Color Color { get; set; }
 
         public virtual void Draw(Graphics graphics, int cellSize)
